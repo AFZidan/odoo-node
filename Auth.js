373a114edb20b28105a9ea2ad3aa8f3ed765ea5b
Ahmed Zidan <dev.ahmed.zidan@gmail.com>
2023-01-01 00:20:55 +0200

    Validate HMAC length bug fixed

--- Auth.js
+++ Auth.js
@@ -1,12 +1,24 @@
 import { createHmac, timingSafeEqual } from 'crypto';
 
+const isLengthEqual = (digestA, digestB) => {
+	if (digestA.length !== digestB.length) {
+		return false;
+	}
+  
+	for (let i = 0; i < digestA.length; i++) {
+		if (Buffer.from(digestA)[i] !== Buffer.from(digestB)[i]) {
+			return false;
+		}
+	}
+  
+	return true;
+};
 export const hasValidHmac = async (req, res, next) => {
 	const hmac = req.headers?.['x-hmac-sha256'] || req.query?.hmac || '';
 	const appSecret = process.env?.APP_SECRET || '';
 	if(!hmac){
 		return res.status(401).json({error: 'Not Authorized'});
 	}
-
 	try{
 		let body = '';
 		if (req.body && Object.keys(req.body).length) {
@@ -19,7 +31,7 @@ export const hasValidHmac = async (req, res, next) => {
 		const computedHmacBuffer = Buffer.from(computedHmac, 'base64');
 		const retrievedSigBuffer = Buffer.from(hmac, 'base64');
   
-		if (!timingSafeEqual(computedHmacBuffer, retrievedSigBuffer)) {
+		if (!isLengthEqual(hmac,computedHmac) || !timingSafeEqual(computedHmacBuffer, retrievedSigBuffer)) {
 			return res.status(401).send('Invalid signature');
 		} 
 		return next();
