6c703133bf5faaa6055e4a3679076d0cd021b9e9
Ahmed Zidan <dev.ahmed.zidan@gmail.com>
2022-12-31 23:21:56 +0200

    Validate hmac improved

--- Auth.js
+++ Auth.js
@@ -1,28 +1,32 @@
 import { createHmac, timingSafeEqual } from 'crypto';
 
 export const hasValidHmac = async (req, res, next) => {
+	const hmac = req.headers?.['x-hmac-sha256'] || req.query?.hmac || '';
+	const appSecret = process.env?.APP_SECRET || '';
+	if(!hmac){
+		return res.status(401).json({error: 'Not Authorized'});
+	}
+
 	try{
 		let body = '';
 		if (req.body && Object.keys(req.body).length) {
 			body = JSON.stringify(req.body);
 		}
-		const signature = `${req.headers['x-hmac-sha256']}`;
-		if(!signature){
-			return res.status(401).json({error: 'Not Authorized'});
-		}
-		const computedSign = await createHmac('sha256', process.env?.APP_SECRET || '')
+		
+		const computedHmac = createHmac('sha256', appSecret)
 			.update(body, 'utf8').digest('base64');
-		console.log('Generated Hash: ',computedSign);
-		const computedSignBuffer = Buffer.from(computedSign, 'base64');
-		const retrievedSigBuffer = Buffer.from(signature, 'base64');
+		console.log('Generated Hash: ',computedHmac);
+		const computedHmacBuffer = Buffer.from(computedHmac, 'base64');
+		const retrievedSigBuffer = Buffer.from(hmac, 'base64');
   
-		if (!timingSafeEqual(computedSignBuffer, retrievedSigBuffer)) {
+		if (!timingSafeEqual(computedHmacBuffer, retrievedSigBuffer)) {
 			return res.status(401).send('Invalid signature');
 		} 
 		return next();
 		
 	}catch(err){
-		console.log(err);
+		console.error(err);
+		return err;
 	}
 
   
